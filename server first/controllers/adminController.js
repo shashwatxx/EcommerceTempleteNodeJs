f8acb4609933d5c48f684e837e2c474aba4ba9bd
shashwatxx <shashwatmishra2111@gmail.com>
2020-09-05 12:35:32 +0530

    small chnge

--- server first/controllers/adminController.js
+++ server first/controllers/adminController.js
@@ -85,6 +85,7 @@ exports.getProducts = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
+
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findByPk(prodId)
