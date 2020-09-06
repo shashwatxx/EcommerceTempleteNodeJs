6a761cd4c58985544a96a1fad7c83c88f25c1744
shashwatxx <shashwatmishra2111@gmail.com>
2020-09-07 02:27:51 +0530

    Auto commit

--- server first/util/database.js
+++ server first/util/database.js
@@ -3,10 +3,10 @@ const mongodb = require("mongodb");
 ///////////////////////////////////
 const MongoClient = mongodb.MongoClient;
 const uri =
-  "mongodb+srv://shashwat:2eAxUNsTSMK3N3Um@cluster0.ofh41.mongodb.net/<dbname>?retryWrites=true&w=majority";
+  "mongodb+srv://shashwat:2eAxUNsTSMK3N3Um@cluster0.ofh41.mongodb.net/test?retryWrites=true&w=majority";
 
 const mongoconnect = (callback) => {
-  MongoClient.connect(uri)
+  MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((client) => {
       console.log("Connected");
       callback(client);
