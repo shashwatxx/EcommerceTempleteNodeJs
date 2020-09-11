c4e947c95563303730b23692b6418d6be123caed
shashwatxx <shashwatmishra2111@gmail.com>
2020-09-12 03:57:59 +0530

    trialagain fhir se

--- server first/models/user.js
+++ server first/models/user.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     name: { type: String, required: true },
+
     email: { type: String, required: true },
     cart: {
         items: [
