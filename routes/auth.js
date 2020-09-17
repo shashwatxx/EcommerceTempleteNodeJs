6af608de7c3b1f414e99834defa52b7c71cd8ba7
shashwatxx <shashwatmishra2111@gmail.com>
2020-09-18 05:11:40 +0530

    Auto Commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
@@ -12,8 +12,17 @@ router.post('/logout', authController.postLogout);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/signup', check('email').isEmail()
-    .withMessage("Please Enter a Valid Email")
+router.post('/signup', [
+    check('email').isEmail()
+        .withMessage("Please Enter a Valid Email"),
+    body('password', "Password Should be of atleast 6 Characters ").isLength({ min: 6 })
+    , body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error("Passwords do not match!!");
+        }
+        return true;
+    })
+]
     , authController.postSignup);
 
 router.get('/resetPass', authController.getReset);
