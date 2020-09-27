2d18e41b0275f69f01ca787d996c5d967e314347
shashwatxx <shashwatmishra2111@gmail.com>
2020-09-27 12:32:46 +0530

    Async requests client side completed on deleteProduct

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,9 +1,9 @@
-const { Result } = require("express-validator");
+
 
 const deleteProduct = (btn) => {
     const prodId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
-
+    const productElement = btn.closest('article');
     fetch('/admin/product/' + prodId, {   //fetch() is used for both Fetching and sending
         method: "DELETE",
         headers: {
@@ -11,7 +11,14 @@ const deleteProduct = (btn) => {
         }
 
     })
-        .then(result => console.log(result))
+        .then(result => {
+            return result.json();
+
+        }).then(data => {
+            console.log(data);
+            // productElement.remove(); dosen't work for Internet Explorer
+            productElement.parentNode.removeChild(productElement);
+        })
         .catch(err => console.log(err))
 
 
