764d96b7d0f770822cc305ec25ed096b633dbf27
shashwatxx <shashwatmishra2111@gmail.com>
2020-09-27 13:32:01 +0530

    payment Method Init

--- routes/shop.js
+++ routes/shop.js
@@ -13,6 +13,7 @@ router.get("/cart", isAuth, shopController.getCart);
 router.post("/cart", isAuth, shopController.postCart);
 router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
 router.post("/create-order", isAuth, shopController.postOrder);
+router.get('/checkout',isAuth,shopController.getChekout)
 router.get("/orders", isAuth, shopController.getOrders);
 router.get('/orders/:orderId',isAuth,shopController.getInvoice);
 // router.get("/checkout", shopController.getCheckout);
